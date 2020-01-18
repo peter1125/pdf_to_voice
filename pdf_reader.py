68b9b162845f81fbe3366b018deab6e627dc21e2
Peter Choi <peter1125@gmail.com>
2020-01-18 15:47:24 -0500

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,15 +1,17 @@
 #extract text from pdf and read it using google text to speech
+
 import PyPDF2, os
 from gtts import gTTS
 from sys import argv
 
+
 pdf_name = argv[1]
-txt_name = argv[2]
-mp3_name = argv[3]
+txt_name = str(pdf_name.split("/")[-1].split(".")[-2]) + ".txt"
+mp3_name = str(pdf_name.split("/")[-1].split(".")[-2]) + ".mp3"
 
 
 def text_extractor():
-    with open(f'{pdf_name}.pdf','rb') as pdf_file, open(f'{txt_name}.txt', 'w',encoding='utf-8') as text_file:
+    with open(f'{pdf_name}','rb') as pdf_file, open(f'{txt_name}', 'w',encoding='utf-8') as text_file:
         pdf_reader = PyPDF2.PdfFileReader(pdf_file)
         all_pages = pdf_reader.getNumPages()
         for page_number in range(all_pages):   
@@ -19,13 +21,12 @@ def text_extractor():
 
 
 def text_to_speech():
-    file_to_read = open(f'{txt_name}.txt', 'r')
+    file_to_read = open(f'{txt_name}', 'r')
     text = file_to_read.read()
     language = 'en'
     output = gTTS(text = text, lang=language, slow=False)
-    output.save(f'{mp3_name}.mp3')
+    output.save(f'{mp3_name}')
     file_to_read.close()
-    os.system(f'start {mp3_name}.mp3')
 
 
 text_extractor()
